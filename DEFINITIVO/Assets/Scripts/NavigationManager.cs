88b341456565707a03a0a99a5b503c1d89a63e08
Matheus Gamba De Oliveira <matheusgamba@furb.br>
2025-08-01 17:24:25 -0300

    Leves melhorias

--- DEFINITIVO/Assets/Scripts/NavigationManager.cs
+++ DEFINITIVO/Assets/Scripts/NavigationManager.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class NavigationManager : MonoBehaviour
 {
-    public enum AppState { Principal, Mapa, Mochila, Galeria }
+    public enum AppState { Principal, Mapa, Mochila, Galeria, Exploracao }
     public AppState currentState;
 
     public GameObject telaPrincipal;
     public GameObject telaMapa;
     public GameObject telaMochila;
     public GameObject telaGaleria;
+    public GameObject telaExploracao; // Novo campo adicionado
 
     void Start()
     {
@@ -23,6 +24,7 @@ public class NavigationManager : MonoBehaviour
         telaMapa.SetActive(false);
         telaMochila.SetActive(false);
         telaGaleria.SetActive(false);
+        telaExploracao.SetActive(false); // Nova linha adicionada
 
         // Ativa apenas a tela desejada
         switch (newState)
@@ -39,6 +41,9 @@ public class NavigationManager : MonoBehaviour
             case AppState.Galeria:
                 telaGaleria.SetActive(true);
                 break;
+            case AppState.Exploracao: // Novo caso adicionado
+                telaExploracao.SetActive(true);
+                break;
         }
 
         currentState = newState;
@@ -49,4 +54,5 @@ public class NavigationManager : MonoBehaviour
     public void GoToMapa() => SetState(AppState.Mapa);
     public void GoToMochila() => SetState(AppState.Mochila);
     public void GoToGaleria() => SetState(AppState.Galeria);
+    public void GoToExploracao() => SetState(AppState.Exploracao); // Novo método adicionado
 }
