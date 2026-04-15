af848fb0a29724afb057345643ab390b6d4dc499
Matheus Gamba De Oliveira <matheusgamba@furb.br>
2026-04-15 17:07:28 -0300

    versao sem tela de permissao

---
+++ DEFINITIVO/Assets/Scripts/LinkOpener.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class AbrirLink : MonoBehaviour
+{
+    public void AbrirTermos()
+    {
+        Application.OpenURL("https://docs.google.com/document/d/1MH16kzG_R-FJvwy7Z1ySn9cy3NdWWsGxqhFffbuotiA/edit?usp=sharing");
+    }
+}
